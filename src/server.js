a9ad23d1f728823f8932926ad79dba15d9c681f4
Erik-EFL <erik.lima.f@outlook.com>
2022-07-17 02:53:37 -0300

    feat: :sparkles: Adicionando middleware
    Adicionei uma importação ao server.js do middleware de erro para poder utiliza-lo me todas as rotas do projeto

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const app = require('./api');
+const { routes } = require('./database/routes/export.routes');
+const middlewareError = require('./middleware/error/middleware.error');
+// const middlewareAuth = require('./middleware/auth/middleware.auth');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -8,5 +11,9 @@ const port = process.env.API_PORT || 3000;
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/user', routes.user);
+
+app.use(middlewareError);
 
 app.listen(port, () => console.log('ouvindo porta', port));
