aa17b49182822e8b846f321cbae2bd8cac5ba18f
Erik-EFL <erik.lima.f@outlook.com>
2022-07-19 16:51:50 -0300

    feat(Controller): :sparkles: Adicionando function getOne
    Criando a função getOne para retornar a resposta do usario pesquisado por ID no bando de dados

--- src/database/controllers/user.controller.js
+++ src/database/controllers/user.controller.js
@@ -16,6 +16,12 @@ const userController = {
     const user = await userService.getAll();
     res.status(200).json(user);
   },
+
+  getOne: async (req, res) => {
+    const { id } = req.params;
+    const user = await userService.getOne(id);
+    res.status(200).json(user);
+  },
 };
 
 module.exports = { userController };
