4c28d86ffd6c789223fc9f9232a05ab8f9d1945d
Erik-EFL <erik.lima.f@outlook.com>
2022-07-19 19:45:18 -0300

    fix(Generator): :bug: Correção de um parametro
    Corrigndo parametro passado sem necessidade para a criação do token

--- src/middleware/generator/generate.jwt.js
+++ src/middleware/generator/generate.jwt.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 const jwt = require('jsonwebtoken');
 
 const generate = (user) => {
-  const { password, ...userResponse } = user;
+  const { password, image, ...userResponse } = user;
   const token = jwt.sign({ id: userResponse }, process.env.JWT_SECRET);
   return token;
 };
