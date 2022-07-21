5bb2c9cf9345fc44408e2db3b3fda8055eb2e81a
Erik-EFL <erik.lima.f@outlook.com>
2022-07-21 01:30:06 -0300

    feat(Service): :sparkles: Função para pesquisar por id
    A função recebe um id retorna a categoria, verifica se a mesma existe e caso exita retorna os postes que contem aquele tema

--- src/database/services/category.services.js
+++ src/database/services/category.services.js
@@ -1,16 +1,26 @@
-const { Category } = require('../models');
+const check = require('../../middleware/validations/verification.error');
+const db = require('../models');
 
 const categoryService = {
   post: {
     create: async (data) => {
-      const newCategory = await Category.create(data);
+      const newCategory = await db.Category.create(data);
       return newCategory;
     },
   },
   get: {
     all: async () => {
-      const categories = await Category.findAll();
+      const categories = await db.Category.findAll();
       return categories;
+    },
+
+    one: async (id) => {
+      console.log(id);
+      const category = await db.Category.findOne({
+        where: { id },
+      });
+
+      check.posts.category.ifExist(category);
     },
   },
 };
