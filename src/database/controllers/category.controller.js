43764f2d48970974c3e38db8b0fc232c155a53a6
Erik-EFL <erik.lima.f@outlook.com>
2022-07-21 01:23:39 -0300

    fix(Controller): :ambulance: Feito ajuste no nome do parametro
    Corrigido nome do parametro que levava a uma interpretação errada do que a função faz

--- src/database/controllers/category.controller.js
+++ src/database/controllers/category.controller.js
@@ -1,10 +1,10 @@
-const { validade } = require('../../middleware/validations/validate');
+const { validate } = require('../../middleware/validations/validate');
 const categoryService = require('../services/category.services');
 
 const categoryController = {
   post: {
     create: async (req, res) => {
-      const { name } = validade.category.body.name(req.body);
+      const { name } = validate.category.body.name(req.body);
       const newCategory = await categoryService.post.create({ name });
       res.status(201).json(newCategory);
     },
