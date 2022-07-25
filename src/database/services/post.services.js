b2a03eb440a57dffdc1e79f975c3a80f62bc7ddd
Erik-EFL <erik.lima.f@outlook.com>
2022-07-25 00:44:39 -0300

    feat(Service): :sparkles: Adicionando novas features a camada de Services/Posts
    Adicionando funções de listar todos os postes, listar por id, criar um novo post, editar um post existente, e suas respectivas verificações e validações.

--- src/database/services/post.services.js
+++ src/database/services/post.services.js
@@ -2,21 +2,61 @@ const Sequelize = require('sequelize');
 const db = require('../models');
 const { PostCategory } = require('../models');
 const config = require('../config/config');
+const check = require('../../middleware/validations/verification.error');
 
 const sequelize = new Sequelize(config.development);
 
 const postService = {
+  get: {
+    all: async () => {
+      const posts = await db.BlogPost.findAll({
+        include: [{ model: db.User, as: 'user', attributes: { exclude: ['password'] } },
+          { model: db.Category,
+            through: { attributes: [] },
+            as: 'Category',
+            attributes: { exclude: ['createdAt', 'updatedAt'] } }],
+      });
+      return posts;
+    },
+    one: async (id) => {
+      const post = await db.BlogPost.findOne({
+        where: { id },
+        include: [{ model: db.User, as: 'user', attributes: { exclude: ['password'] } },
+          { model: db.Category,
+            through: { attributes: [] },
+            as: 'Category',
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
+          }],
+      });
+      console.log('SOU UM MERDA 30', post);
+      return post;
+    },
+  },
   post: {
     create: async ({ data, userId }) => {
       const { title, content, categoryIds } = data;
-      const newPost = await sequelize.transaction(async (t) => {
-        const post = await db.BlogPost.create({ title, content, userId }, { t });
+      const newPost = await sequelize.transaction(async (transaction) => {
+        const post = await db.BlogPost.create({ title, content, userId }, { transaction });
         await Promise.all(categoryIds.map(
-          (id) => PostCategory.create({ postId: post.id, categoryId: id }, { t }),
+          (id) => PostCategory.create({ postId: post.id, categoryId: id }, { transaction }),
           ));
         return post;
       });
       return newPost;
+    },
+  },
+  put: {
+    edit: async ({ data, userId }) => {
+      const { title, content } = data;
+      const editedPost = await db.BlogPost.update(
+          { title, content }, { where: { userId } },
+          );
+      return editedPost;
+    },
+  },
+  delete: {
+    one: async (postId) => {
+      await db.BlogPost.destroy({ where: { id: postId } });
     },
   },
 };
